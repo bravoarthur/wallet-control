051953f7f2767f0d19e07069934d9b6dd62b6b81
bravoarthur <arthur.bravo08@gmail.com>
2022-05-13 13:47:44 +1000

    Progress Test home

--- src/pages/NotFound.test.tsx
+++ src/pages/NotFound.test.tsx
@@ -1,4 +1,4 @@
-import {render,  screen} from "@testing-library/react"
+import {fireEvent, render,  screen} from "@testing-library/react"
 import NotFound from "./NotFound"
 
 
@@ -14,9 +14,16 @@ describe('Page Not Found', () => {
     it('Shows Text "Not Found"', () => {
         render(<NotFound/>)
 
-        expect(screen.getByTestId('h1NotFound')).toBeInTheDocument()
+        expect(screen.getByTestId('h1NotFound')).toBeInTheDocument()        
+    })
+    test('Function "back home" is called when click button', () => {
 
+        render(<NotFound/>)
+        const button = screen.getByText('Home Page')
+        fireEvent.click(button)
         
+        expect(mockedUsedNavigate).toHaveBeenCalled()
+
     })
 
 })
